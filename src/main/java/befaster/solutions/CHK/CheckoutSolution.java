a64887f9e1fb4ab9ad66f114b0689acf36c809c6
sbx_user1051 <sbx_user1051@169.254.28.133>
2023-11-05 14:34:05 +0000

    Sun Nov 05 14:34:05 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -12,16 +12,36 @@ public class CheckoutSolution {
     private Map<Character, List<Offer>> offers;
     private Map<Character, FreeItemOffer> freeItemOffers;
 
-    //+------+-------+------------------------+
-    //| Item | Price | Special offers         |
-    //+------+-------+------------------------+
-    //| A    | 50    | 3A for 130, 5A for 200 |
-    //| B    | 30    | 2B for 45              |
-    //| C    | 20    |                        |
-    //| D    | 15    |                        |
-    //| E    | 40    | 2E get one B free      |
-    //+------+-------+------------------------+
-    //| F    | 10    | 2F get one F free      |
+//+------+-------+------------------------+
+//| Item | Price | Special offers         |
+//+------+-------+------------------------+
+//| A    | 50    | 3A for 130, 5A for 200 |
+//| B    | 30    | 2B for 45              |
+//| C    | 20    |                        |
+//| D    | 15    |                        |
+//| E    | 40    | 2E get one B free      |
+//| F    | 10    | 2F get one F free      |
+//| G    | 20    |                        |
+//| H    | 10    | 5H for 45, 10H for 80  |
+//| I    | 35    |                        |
+//| J    | 60    |                        |
+//| K    | 80    | 2K for 150             |
+//| L    | 90    |                        |
+//| M    | 15    |                        |
+//| N    | 40    | 3N get one M free      |
+//| O    | 10    |                        |
+//| P    | 50    | 5P for 200             |
+//| Q    | 30    | 3Q for 80              |
+//| R    | 50    | 3R get one Q free      |
+//| S    | 30    |                        |
+//| T    | 20    |                        |
+//| U    | 40    | 3U get one U free      |
+//| V    | 50    | 2V for 90, 3V for 130  |
+//| W    | 20    |                        |
+//| X    | 90    |                        |
+//| Y    | 10    |                        |
+//| Z    | 50    |                        |
+//+------+-------+------------------------+
 
     public CheckoutSolution() {
         prices = new HashMap<>();
@@ -31,14 +51,42 @@ public class CheckoutSolution {
         prices.put('D', 15);
         prices.put('E', 40);
         prices.put('F', 10);
-
+        prices.put('G', 20);
+        prices.put('H', 10);
+        prices.put('I', 35);
+        prices.put('J', 60);
+        prices.put('K', 80);
+        prices.put('L', 90);
+        prices.put('M', 15);
+        prices.put('N', 40);
+        prices.put('O', 10);
+        prices.put('P', 50);
+        prices.put('Q', 30);
+        prices.put('R', 50);
+        prices.put('S', 30);
+        prices.put('T', 20);
+        prices.put('U', 40);
+        prices.put('V', 50);
+        prices.put('W', 20);
+        prices.put('X', 90);
+        prices.put('Y', 10);
+        prices.put('Z', 50);
+        
         offers = new HashMap<>();
         offers.put('A', List.of(new Offer(5, 200), new Offer(3, 130)));
         offers.put('B', List.of(new Offer(2, 45)));
         offers.put('F', List.of(new Offer(3, 20)));
+        offers.put('H', List.of(new Offer(10, 80), new Offer(5, 45)));
+        offers.put('K', List.of(new Offer(2, 150)));
+        offers.put('P', List.of(new Offer(5, 200)));
+        offers.put('Q', List.of(new Offer(3, 80)));
+        offers.put('U', List.of(new Offer(4, 120)));
+        offers.put('V', List.of(new Offer(3, 130), new Offer(2, 90)));
 
         freeItemOffers = new HashMap<>();
         freeItemOffers.put('E', new FreeItemOffer(2, 'B'));
+        freeItemOffers.put('N', new FreeItemOffer(3, 'M'));
+        freeItemOffers.put('R', new FreeItemOffer(3, 'Q'));
     }
 
     public Integer checkout(String skus) {
@@ -94,4 +142,5 @@ public class CheckoutSolution {
         return total;
     }
 }
+
 
